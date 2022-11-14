11965846b446fe549eb3c89ba6c00e9819b37718
weshakvoort <weshakvoort@gmail.com>
2022-11-14 21:54:06 +0100

    winkelmand style gefixt en item kan gewijzigt worden

---
+++ functionsWinkelmand.php
@@ -0,0 +1,67 @@
+<?php
+
+//functie haalt de sessie inhoud op
+function getCart()
+{
+    if (isset($_SESSION['cart'])) {               //controleren of winkelmandje (=cart) al bestaat
+        $cart = $_SESSION['cart'];                  //zo ja:  ophalen
+    } else {
+        $cart = array();                            //zo nee: dan een nieuwe (nog lege) array
+    }
+    return $cart;                               // resulterend winkelmandje terug naar aanroeper functie
+}
+
+// functie slaat de winkelmand inhoud op in de sessie
+function saveCart($cart)
+{
+    $_SESSION["cart"] = $cart;// werk de "gedeelde" $_SESSION["cart"] bij met de meegestuurde gegevens
+}
+
+// functie voegt 1 per keer van het aantal toe voor het product
+function addProductToCart($stockItemID)
+{
+    $cart = getCart();                          // eerst de huidige cart ophalen
+
+    if (array_key_exists($stockItemID, $cart)) {  //controleren of $stockItemID(=key!) al in array staat
+        $cart[$stockItemID] += 1;                   //zo ja:  aantal met 1 verhogen
+    } else {
+        $cart[$stockItemID] = 1;                    //zo nee: key toevoegen en aantal op 1 zetten.
+    }
+
+    saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
+}
+
+// functie haalt 1 per keer van het aantal af voor het product
+function removeProductFromCart($stockItemID)
+{
+    $cart = getCart();                          // eerst de huidige cart ophalen
+
+    if (array_key_exists($stockItemID, $cart)) {  //controleren of $stockItemID(=key!) al in array staat
+        $cart[$stockItemID] -= 1;                   //zo ja: aantal met 1 verlagen
+    } else {
+        $cart[$stockItemID] = 1;                    //zo nee: key toevoegen en aantal op 1 zetten.
+    }
+
+    saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
+}
+
+// functie zorgt er voor dat een item verwijderd wordt uit de sessie
+function deleteProductFromCart($stockItemID){
+    $cart = getCart();
+    if (array_key_exists($stockItemID, $cart)) {
+        unset($cart[$stockItemID]);
+    }
+    saveCart($cart);
+}
+
+// berekend de totaal prijs van het winkelmandje
+function totaal_prijs($StockItem, $cart)
+{
+    $totaal_prijs = 0;
+    foreach ($cart as $value) {
+        $key_int = array_sum($cart);
+        $StockItem_int = floatval($StockItem['SellPrice']);
+        $totaal_prijs = $key_int * $StockItem_int;
+    }
+    return $totaal_prijs;
+}
--- winkelmand.php
+++ winkelmand.php
@@ -8,30 +8,17 @@ $cart = getCart();
 $StockItem = getStockItem($cart, $databaseConnection);
 $StockItemImage = getStockItemImage($cart, $databaseConnection);
 
-// berekend de totaal prijs van het winkelmandje
-function totaal_prijs($StockItem, $cart)
-{
-    $totaal_prijs = 0;
-    foreach ($cart as $value) {
-        $key_int = array_sum($cart);
-        $StockItem_int = floatval($StockItem['SellPrice']);
-        $totaal_prijs = $key_int * $StockItem_int;
-    }
-    return $totaal_prijs;
-}
-
-
 ?>
 
 
 <!-- code deel 3 van User story: Zoeken producten : de html -->
 <!-- de zoekbalk links op de pagina  -->
 
-<script>
-    if (window.history.replaceState) {
-        window.history.replaceState(null, null, window.location.href);
-    }
-</script>
+<!--<script>-->
+<!--    if (window.history.replaceState) {-->
+<!--        window.history.replaceState(null, null, window.location.href);-->
+<!--    }-->
+<!--</script>-->
 
 <div id="FilterFrame"><h2 class="FilterText"><i class="fa-solid fa-cart-shopping"></i> Winkelmand </h2>
     <form>
@@ -51,110 +38,115 @@ function totaal_prijs($StockItem, $cart)
 
 <!-- einde zoekresultaten die links van de zoekbalk staan -->
 <!-- einde code deel 3 van User story: Zoeken producten  -->
+<div style="margin-left: 21%;">
+<?php
+$i =0;
+foreach ($cart
 
-<div id="ResultsArea" class="Browse">
+as $key => $value) {
+if ($key == "") {
+    continue;
+} else {
+    $naam = getStockItem($key, $databaseConnection);
+    $i++;
+}
+?>
+<div id="ResultsAreaWinkelmandje" class="Browse">
     <br>
     <div id="ArticleHeader">
-        <?php
-        foreach ($cart as $key => $value) {
-            if($key == "") {
-                continue;
-            } else {
-                $naam = getStockItem($key, $databaseConnection);
+            <?php
+            $foto = getStockItemImage($key, $databaseConnection);
+            if (isset($foto)) {
+                // één plaatje laten zien
+                if (count($foto) == 1) {
+                    ?>
+                    <div id="ImageFrame"
+                         style="background-image: url('Public/StockItemIMG/<?php print $foto[0]['ImagePath']; ?>'); background-size: 300px; background-repeat: no-repeat; background-position: center;"></div>
+                    <?php
+                } else { ?>
+                    <!-- zorgt voor de back-up  -->
+                    <div id="ImageFrame"
+                         style="background-image: url('Public/StockGroupIMG/<?php print $StockItem['BackupImagePath']; ?>'); background-size: cover;">
+                    </div>
+                    <?php
+                }
             }
             ?>
-            <div class="ListItem">
-                <?php
-                $foto = getStockItemImage($key, $databaseConnection);
-                if (isset($foto)) {
-                    // één plaatje laten zien
-                    if (count($foto) == 1) {
-                        ?>
-                        <div id="ImageFrame"
-                             style="background-image: url('Public/StockItemIMG/<?php print $foto[0]['ImagePath']; ?>'); background-size: 300px; background-repeat: no-repeat; background-position: center;"></div>
-                        <?php
-                    } else { ?>
-                        <!-- zorgt voor de back-up  -->
-                        <div id="ImageFrame"
-                             style="background-image: url('Public/StockGroupIMG/<?php print $StockItem['BackupImagePath']; ?>'); background-size: cover;">
-                        </div>
-                        <?php
-                    }
-                }
-                ?>
 
-                <div>
-                    <!-- Print het artikelnummer -->
-                    <h1 class="StockItemID">Artikelnummer: <?php print $key; ?></h1>
-                    <!-- Print de naam van het item -->
-                    <h2 class="StockItemNameViewSize StockItemName">
-                        <?php print $naam['StockItemName']; ?>
-                    </h2>
-                    <!-- Prijs, hoeveelheid en inclusief btw -->
-                    <div class="QuantityText">
-                        <!-- Print de per stuk prijs -->
-                        <p class="StockItemPriceText"><b><?php print sprintf("€ %.2f", $naam['SellPrice']); ?></b>
-                        </p>
-                        <!-- Verzorgd hoeveel items er in het winkelmandje zitten -->
-                        <div class="input-group inputGroup-sizing-sm mb-3" style="width: 175px">
-                            <!-- Form met post method om de hoeveelheid van een item te verlagen -->
-                            <form method="post">
-                                <input type="number" name="Decrease" value="<?php print($naam["StockItemID"]) ?>"
-                                       hidden>
-                                <button class="btn btn-outline-secondary" type="submit" name="submitDecrease"
-                                        id="button-addon1">
-                                    <i class="fa-solid fa-circle-minus"></i>
-                                </button>
-                                <?php
-                                if (isset($_POST["submitDecrease"])) { // zelfafhandelend formulier
-                                    $stockItemID = $_POST["submitDecrease"];
-                                    removeProductFromCart($stockItemID); // maak gebruik van geïmporteerde functie uit header.php
-                                }
-                                ?>
-                            </form>
-                            <!-- Laten zien wat het huidige aantal is -->
-                            <input type="text" class="form-control p-0" placeholder=""
-                                   aria-label="Example text with button addon"
-                                   aria-describedby="button-addon1"
-                                   value=" <?php print array_sum($cart); //aantal wat in winkel wagen zit?>">
-                            <!-- Form met post method om de hoeveelheid van een item te verhogen -->
-                            <form method="post">
-                                <input type="number" name="Increase" value="<?php print($naam["StockItemID"]) ?>"
-                                       hidden>
-                                <button class="btn btn-outline-secondary" type="submit"
-                                        name="submitIncrease" id="button-addon2">
-                                    <i class="fa-solid fa-circle-plus"></i>
-                                </button>
-                                <?php
-                                if (isset($_POST["submitIncrease"])) { // zelfafhandelend formulier
-                                    $stockItemID = $_POST["submitIncrease"];
-                                    addProductToCart($stockItemID); // maak gebruik van geïmporteerde functie uit Header.php
-                                } ?>
-                            </form>
-                            <!-- Form met post method om de hoeveelheid van een item te verwijderen -->
-                            <form method="post" class="pr-2">
-                                <input type="number" name="Increase" value="<?php print($naam["StockItemID"]) ?>"
-                                       hidden>
-                                <button class="btn btn-outline-danger" type="submit"
-                                        name="delete" id="button-addon2">
-                                    <i class="fa-solid fa-trash"></i>
-                                </button>
-                                <?php
-                                if (isset($_POST["delete"])) { // zelfafhandelend formulier
-                                    $stockItemID = $_POST["delete"]; //
-                                    removeProductFromCart($stockItemID);
-                                    // maak gebruik van geïmporteerde functie uit Header.php
-                                } ?>
-                            </form>
-                        </div>
-                        <h6> Inclusief BTW </h6>
-                        <?php print $naam['QuantityOnHand']; ?>
+            <div>
+                <!-- Print het artikelnummer -->
+                <h1 class="StockItemID">Artikelnummer: <?php print $key; ?></h1>
+                <!-- Print de naam van het item -->
+                <h2 class="StockItemNameViewSize StockItemName">
+                    <?php print $naam['StockItemName']; ?>
+                </h2>
+                <!-- Prijs, hoeveelheid en inclusief btw -->
+                <div class="QuantityText">
+                    <!-- Print de per stuk prijs -->
+                    <p class="StockItemPriceText"><b><?php print sprintf("€ %.2f", $naam['SellPrice']); ?></b>
+                    </p>
+                    <!-- Verzorgd hoeveel items er in het winkelmandje zitten -->
+                    <div class="input-group inputGroup-sizing-sm mb-3" style="width: 175px">
+                        <!-- Form met post method om de hoeveelheid van een item te verlagen -->
+                        <form method="post" onSubmit="window.location.reload()">
+                            <input type="number" name="Decrease" value="<?php print($naam["StockItemID"]) ?>"
+                                   hidden>
+                            <button class="btn btn-outline-secondary" type="submit" name="submitDecrease"
+                                    id="button-addon1">
+                                <i class="fa-solid fa-circle-minus"></i>
+                            </button>
+                            <?php
+                            if (isset($_POST["Decrease"])) { // zelfafhandelend formulier
+                                $stockItemID = $_POST["Decrease"];
+                                removeProductFromCart($stockItemID); // maak gebruik van geïmporteerde functie uit header.php
+                                print ' <meta http-equiv="refresh" content="0">';
+                            }
+                            ?>
+                        </form>
+                        <!-- Laten zien wat het huidige aantal is -->
+                        <input type="text" class="form-control p-0" placeholder=""
+                               aria-label="Example text with button addon"
+                               aria-describedby="button-addon1"
+                               value=" <?php print $cart[$key]; //aantal wat in winkel wagen zit?>">
+                        <!-- Form met post method om de hoeveelheid van een item te verhogen -->
+                        <form method="post">
+                            <input type="number" name="Increase" value="<?php print($naam["StockItemID"]) ?>"
+                                   hidden>
+                            <button class="btn btn-outline-secondary" type="submit"
+                                    name="submitIncrease" id="button-addon2">
+                                <i class="fa-solid fa-circle-plus"></i>
+                            </button>
+                            <?php
+                            if (isset($_POST["Increase"])) { // zelfafhandelend formulier
+                                $stockItemID = $_POST["Increase"];
+                                addProductToCart($stockItemID); // maak gebruik van geïmporteerde functie uit Header.php
+                                print ' <meta http-equiv="refresh" content="0">';
+                            } ?>
+                        </form>
+                        <!-- Form met post method om de hoeveelheid van een item te verwijderen -->
+                        <form method="post" class="pr-2">
+                            <input type="number" name="Delete" value="<?php print($naam["StockItemID"]) ?>"
+                                   hidden>
+                            <button class="btn btn-outline-danger" type="submit"
+                                    name="delete2" id="button-addon2">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                            <?php
+                            if (isset($_POST["Delete"])) { // zelfafhandelend formulier
+                                $stockItemID = $_POST["Delete"]; //
+                                deleteProductFromCart($stockItemID); // maak gebruik van geïmporteerde functie uit Header.php
+                                print ' <meta http-equiv="refresh" content="0">';
+                            } ?>
+                        </form>
                     </div>
+                    <h6> Inclusief BTW </h6>
+                    <?php print $naam['QuantityOnHand']; ?>
                 </div>
             </div>
-        <?php } ?>
     </div>
+        <?php } ?>
 
+</div>
     <?php
     include __DIR__ . "/footer.php";
     ?>
