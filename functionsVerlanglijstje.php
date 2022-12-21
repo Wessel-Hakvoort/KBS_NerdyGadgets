337eab6c521006d906fa261237ed6a269e4f3797
Wouter Hielkema <wouter.hielkema@gmail.com>
2022-12-21 14:29:10 +0100

    error issue gefixt. login bij niet ingelogd hyperlink gefixt. accounts weggehaald bij login.php. sql check toegevoegd in functionsVerlanlijstje

--- functionsVerlanglijstje.php
+++ functionsVerlanglijstje.php
@@ -5,12 +5,25 @@ function databaseVerlanglijstje($connection, $stockItemID)
     if ($_SESSION["loggedin"] == TRUE) {
         $userid = $_SESSION["id"];
         //zoekt klant gegevens op basis van userid session
-        $result = mysqli_query($connection, "SELECT CustomerID FROM customers WHERE CustomerID = (SELECT CustomerID FROM users WHERE userid = $userid);");
+        $result = mysqli_query($connection, "SELECT CustomerID FROM customers WHERE CustomerID = (SELECT CustomerID FROM users WHERE userid = '$userid');");
         while ($row = mysqli_fetch_array($result)) {
             $CustomerID = $row['CustomerID'];
             $statement = mysqli_prepare($connection, "INSERT INTO verlanglijstje (CustomerID, StockItemID) VALUES('$CustomerID', '$stockItemID')");
             mysqli_stmt_execute($statement);
             return mysqli_stmt_affected_rows($statement) == 1;
+        }
+    }
+}
+
+function ophalenCustomerID($connection)
+{
+    if ($_SESSION["loggedin"] == TRUE) {
+        $userid = $_SESSION["id"];
+        //zoekt klant gegevens op basis van userid session
+        $result = mysqli_query($connection, "SELECT CustomerID FROM customers WHERE CustomerID = (SELECT CustomerID FROM users WHERE userid = '$userid');");
+        while ($row = mysqli_fetch_array($result)) {
+            $CustomerID = $row['CustomerID'];
+            return $CustomerID;
         }
     }
 }
